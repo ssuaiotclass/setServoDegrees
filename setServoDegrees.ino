96f26aa8775b42fc27d75a4963f32d910fe738f5
軟實力精進聯盟 SoftSkillsUnion <softskillsunion@gmail.com>
2021-04-05 05:30:06 +0800

    basic

---
+++ setServoDegrees.ino
@@ -0,0 +1,14 @@
+#include <Servo.h>
+
+int servoPin = 15; // D8
+Servo myservo;     // create servo object to control a servo
+
+void setup()
+{
+    myservo.attach(servoPin); // attaches the servo on D8 to the servo object
+}
+
+void loop()
+{
+    myservo.write(90);
+}
